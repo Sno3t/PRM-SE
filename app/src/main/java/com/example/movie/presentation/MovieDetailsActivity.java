81ce8c2d1c5c0a78980d4fc0d466eb0f45c2d35e
Sno3t <tycho.brakenhoff@ictmbo.nl>
2023-03-21 23:26:36 +0100

    Basic empty files

---
+++ app/src/main/java/com/example/movie/presentation/MovieDetailsActivity.java
@@ -0,0 +1,4 @@
+package com.example.movie.presentation;
+
+public class MovieDetailsActivity {
+}
---
+++ app/src/main/java/com/example/movie/presentation/viemodel/MovieViewModel.java
@@ -0,0 +1,4 @@
+package com.example.movie.presentation.viemodel;
+
+public class MovieViewModel {
+}
