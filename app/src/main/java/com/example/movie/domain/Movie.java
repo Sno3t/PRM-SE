e77a091481b2b94d98055d0358d78ee6f7074dc7
Crovvyboi <81975137+Crovvyboi@users.noreply.github.com>
2023-03-27 01:59:27 +0200

    Setup (broken, pls check)
    Doesn't connect to API properly (can't execute inputstream command on ln. 39 in APIConn.java)

--- app/src/main/java/com/example/movie/domain/Movie.java
+++ app/src/main/java/com/example/movie/domain/Movie.java
@@ -1,4 +1,14 @@
 package com.example.movie.domain;
 
 public class Movie {
+
+    public String title;
+    public String description;
+    public Integer runtime;
+
+    public Movie(String title, String description, Integer runtime){
+        this.title = title;
+        this.description = description;
+        this.runtime = runtime;
+    }
 }
--- app/src/main/java/com/example/movie/presentation/MainActivity.java
+++ app/src/main/java/com/example/movie/presentation/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.movie.presentation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 
 import com.example.movie.R;
+import com.example.movie.domain.APIConn;
+import com.example.movie.domain.Movie;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        APIConn conn = new APIConn(findViewById(R.id.recyclerview));
+        conn.GetMovieByID();
     }
 }
