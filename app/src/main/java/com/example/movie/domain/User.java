32a89ad30ba7c40b58eee3154817408a4bf8dd8a
BBayrkdar <118437053+BBayrkdar@users.noreply.github.com>
2023-03-27 10:18:09 +0200

    User class

--- app/src/main/java/com/example/movie/domain/User.java
+++ app/src/main/java/com/example/movie/domain/User.java
@@ -1,10 +1,44 @@
 package com.example.movie.domain;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class User {
 
+    private String firstName;
+    private String lastName;
+    private Date birthday;
     private ArrayList<MovieList> userLists;
+
+    public User(String firstName, String lastName, Date birthday) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
 
     public User(){
         userLists = new ArrayList<>();
