38d8e22ed6716b3e7179d2abdda0ef4a20551d37
BBayrkdar <118437053+BBayrkdar@users.noreply.github.com>
2023-03-29 10:51:51 +0200

    drawable items en detail layout toevoegen

--- app/src/main/java/com/example/movie/presentation/MovieDetailsActivity.java
+++ app/src/main/java/com/example/movie/presentation/MovieDetailsActivity.java
@@ -1,4 +1,17 @@
 package com.example.movie.presentation;
 
-public class MovieDetailsActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.movie.R;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+    }
 }
