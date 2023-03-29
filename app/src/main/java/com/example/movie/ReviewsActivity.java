b2daa4466bdf81ec45fe81f891a4052c4b02f3e9
BBayrkdar <118437053+BBayrkdar@users.noreply.github.com>
2023-03-29 15:45:55 +0200

    activity reviews aanpassen

--- app/src/main/java/com/example/movie/ReviewsActivity.java
+++ app/src/main/java/com/example/movie/ReviewsActivity.java
@@ -24,6 +24,8 @@ public class ReviewsActivity extends AppCompatActivity {
     }
 
     public void submitReview(View view) {
+        String addedReview = newReview.toString();
+        ratingBar.getRating();
 
     }
 }
