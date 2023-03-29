a96eee2457e870b0fa31452f86a49af06e88d104
BBayrkdar <118437053+BBayrkdar@users.noreply.github.com>
2023-03-29 16:36:43 +0200

    ListActivity and activity_lists added

---
+++ app/src/main/java/com/example/movie/ListsActivity.java
@@ -0,0 +1,20 @@
+package com.example.movie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ListsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lists);
+
+    }
+
+    public void addNewList(View view) {
+
+    }
+}
