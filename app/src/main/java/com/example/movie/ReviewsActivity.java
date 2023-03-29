9d864b626170990f8900497af2cbe966102ea400
BBayrkdar <118437053+BBayrkdar@users.noreply.github.com>
2023-03-29 15:26:31 +0200

    RatingBar, activity reviews en ReviewsActivity toevoegen plus wat aanpassingen en colors toevoegen

---
+++ app/src/main/java/com/example/movie/ReviewsActivity.java
@@ -0,0 +1,29 @@
+package com.example.movie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RatingBar;
+
+public class ReviewsActivity extends AppCompatActivity {
+
+    private EditText newReview;
+    private RatingBar ratingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reviews);
+
+        newReview = findViewById(R.id.new_review_movie);
+        ratingBar = findViewById(R.id.custom_rating_bar);
+
+    }
+
+    public void submitReview(View view) {
+
+    }
+}
