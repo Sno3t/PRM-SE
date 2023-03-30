36cb72b92c5673d5897e59c5e8177a94fc271814
rikoldebijvank <r.oldebijvank@student.avans.nl>
2023-03-30 03:33:49 +0200

    new branch

--- app/src/main/java/com/example/movie/presentation/viemodel/MovieViewModel.java
+++ app/src/main/java/com/example/movie/presentation/viemodel/MovieViewModel.java
@@ -1,4 +1,18 @@
 package com.example.movie.presentation.viemodel;
 
-public class MovieViewModel {
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.movie.domain.Movie;
+
+import java.util.List;
+
+public class MovieViewModel extends AndroidViewModel {
+    private LiveData<List<Movie>> mAllMovies;
+
+    public MovieViewModel(Application application) {
+        super(application);
+    }
 }
